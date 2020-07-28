0c68f4e82ad67759240db643e29711ce10b52361
Stefanos Malliaros <stefanos.malliaros@fetch.ai>
2020-07-28 14:49:09 +0100

    Removed sleeps

--- entrypoints/entrypoint.sh
+++ entrypoints/entrypoint.sh
@@ -3,7 +3,6 @@
 if [ ! $BOOTSTRAP == "" ];
 then
   echo "Fetching configuration for $BOOTSTRAP network"
-  sleep 5
   bootstrapurl="https://raw.githubusercontent.com/fetchai/networks-$BOOTSTRAP/master/bootstrap/boostrap.json"
   export CHAINID=$(curl -sS $bootstrapurl  | (jq -r .chainid))
   export ARGS=$(curl -sS $bootstrapurl  | (jq -r .args))
@@ -14,7 +13,6 @@ CHECK_FILE="/root/secret-temp-config/config/config.toml"
 if [ -f "$CHECK_FILE" ];
 then
   echo "Provided node configuration files in /root/secret-temp-config"
-  sleep 5
   mkdir /root/.wasmd
   cp -R /root/secret-temp-config/* /root/.wasmd/
   curl https://rpc-${CHAINID}.fetch.ai/genesis? | jq .result.genesis > ~/.wasmd/config/genesis.json
@@ -22,7 +20,6 @@ then
   wasmd start --p2p.laddr tcp://0.0.0.0:26656 --rpc.laddr tcp://0.0.0.0:26657 ${ARGS}
 else
   echo "Node configuration files have not been provided"
-  sleep 5
   wasmd init $MONIKER --chain-id ${CHAINID}
   curl https://rpc-${CHAINID}.fetch.ai/genesis? | jq .result.genesis > ~/.wasmd/config/genesis.json
   sed -i  's/allow_duplicate_ip = false/allow_duplicate_ip = true/' ~/.wasmd/config/config.toml
