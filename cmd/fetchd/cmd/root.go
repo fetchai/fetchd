3988af7ac517fb15ca47f0789908b54f00e147c6
daeMOn <flavien.binet@gmail.com>
2021-06-29 09:24:39 +0200

    feat: add stargate-migrate command (#113)
    * add stargate-migrate command
    
    This new command migrate a genesis file from current mainnet version
    (sdk v0.38.3) to the stargate version (v0.42.5)
    
    It handles:
    - Setting default value for consensus.evidences.max_bytes which generate
      a warning if 0
    - Removing multisig pubkeys if present, as they fail to decode properly
      with cosmos-sdk v0.38.3
    - Resetting wasm module state, removing any existing contracts as they
      must be recompile for the newest VM version.
    - Migrate genesis from v0.38 to v0.39, and then v0.39 to v0.40
    - Allow override of genesis time and chain id
    
    * feat: add extra initial_height flag

--- cmd/fetchd/cmd/root.go
+++ cmd/fetchd/cmd/root.go
@@ -92,7 +92,7 @@ func initRootCmd(rootCmd *cobra.Command, encodingConfig params.EncodingConfig) {
 		genutilcli.InitCmd(app.ModuleBasics, app.DefaultNodeHome),
 		config.Cmd(),
 		genutilcli.CollectGenTxsCmd(banktypes.GenesisBalancesIterator{}, app.DefaultNodeHome),
-		genutilcli.MigrateGenesisCmd(),
+		AddStargateMigrateCmd(),
 		genutilcli.GenTxCmd(app.ModuleBasics, encodingConfig.TxConfig, banktypes.GenesisBalancesIterator{}, app.DefaultNodeHome),
 		genutilcli.ValidateGenesisCmd(app.ModuleBasics),
 		AddGenesisAccountCmd(app.DefaultNodeHome),
