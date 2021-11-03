f1231004fb2989d1802065f97e8be147ac271505
Flavien Binet <flavien.binet@gmail.com>
2021-11-03 17:56:23 +0100

    fix: add new iterator to wasmd supported features

--- app/app.go
+++ app/app.go
@@ -364,7 +364,7 @@ func New(
 
 	// The last arguments can contain custom message handlers, and custom query handlers,
 	// if we want to allow any custom callbacks
-	supportedFeatures := "staking,stargate"
+	supportedFeatures := "iterator,staking,stargate"
 	app.wasmKeeper = wasm.NewKeeper(
 		appCodec,
 		keys[wasm.StoreKey],
