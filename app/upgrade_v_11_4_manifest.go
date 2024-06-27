7425b2af621a57b916d4244f639c675424c2d7fe
Joey Sumner <47419781+Jonathansumner@users.noreply.github.com>
2024-06-27 18:50:27 +0100

    Reinstating reconciliation from the PR#375

--- app/upgrade_v_11_4_manifest.go
+++ app/upgrade_v_11_4_manifest.go
@@ -11,6 +11,8 @@ import (
 const manifestFilenameBase = "upgrade_manifest.json"
 
 type UpgradeManifest struct {
+	Reconciliation     *UpgradeReconciliation     `json:"reconciliation,omitempty"`
+	Contracts          *Contracts                 `json:"contracts,omitempty"`
 	IBC                *UpgradeIBCTransfers       `json:"ibc,omitempty"`
 	Migration          *UpgradeMigation           `json:"migration,omitempty"`
 	MoveGenesisBalance *UpgradeMoveGenesisBalance `json:"move_genesis_balance,omitempty"`
@@ -20,6 +22,59 @@ type UpgradeManifest struct {
 
 func NewUpgradeManifest() *UpgradeManifest {
 	return &UpgradeManifest{}
+}
+
+type Contracts struct {
+	StateCleaned   []string                `json:"contracts_state_cleaned,omitempty"`
+	AdminUpdated   []ContractValueUpdate   `json:"contracts_admin_updated,omitempty"`
+	LabelUpdated   []ContractValueUpdate   `json:"contracts_label_updated,omitempty"`
+	VersionUpdated []ContractVersionUpdate `json:"version_updated,omitempty"`
+}
+
+type ContractValueUpdate struct {
+	Address string `json:"address"`
+	From    string `json:"from"`
+	To      string `json:"to"`
+}
+
+type ContractVersionUpdate struct {
+	Address string           `json:"address"`
+	From    *ContractVersion `json:"from,omitempty"`
+	To      *ContractVersion `json:"to"`
+}
+
+type ValueUpdate struct {
+	From string `json:"from"`
+	To   string `json:"to"`
+}
+
+type UpgradeReconciliation struct {
+	Transfers     *UpgradeReconciliationTransfers     `json:"transfers,omitempty"`
+	ContractState *UpgradeReconciliationContractState `json:"contract_state,omitempty"`
+}
+
+type UpgradeReconciliationTransfer struct {
+	From    string      `json:"from"`
+	EthAddr string      `json:"eth_addr"`
+	Amount  types.Coins `json:"amount"`
+}
+
+type UpgradeReconciliationTransfers struct {
+	Transfers                   []UpgradeReconciliationTransfer `json:"transfers"`
+	To                          string                          `json:"to"`
+	AggregatedTransferredAmount types.Coins                     `json:"aggregated_transferred_amount"`
+	NumberOfTransfers           int                             `json:"number_of_transfers"`
+}
+
+type UpgradeReconciliationContractStateBalanceRecord struct {
+	EthAddr  string      `json:"eth_addr"`
+	Balances types.Coins `json:"balances"`
+}
+
+type UpgradeReconciliationContractState struct {
+	Balances                 []UpgradeReconciliationContractStateBalanceRecord `json:"balances"`
+	AggregatedBalancesAmount types.Coins                                       `json:"aggregated_balances_amount"`
+	NumberOfBalanceRecords   int                                               `json:"number_of_balance_records"`
 }
 
 type UpgradeIBCTransfer struct {
