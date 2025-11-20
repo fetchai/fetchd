339601c3afb2feefe62169074a52aa45be79c8a9
Peter Bukva <peter.bukva@gmail.com>
2025-11-20 11:26:59 +0000

    fix: Setting the name for Gemini testner upgrade handler to rc1 git tag

--- app/upgrades.go
+++ app/upgrades.go
@@ -36,7 +36,7 @@ import (
 )
 
 // ---- Match this to the plan name that is already stored on disk and halted the chain.
-const UpgradeNameV053 = "v0.15.0-rc2-gemini"
+const UpgradeNameV053 = "v0.15.0-rc1-gemini"
 
 // List ALL new/renamed/deleted KV stores at this upgrade height.
 var v053StoreUpgrades = storetypes.StoreUpgrades{
