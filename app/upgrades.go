df24aa434015bdbde48b9b0738f8b7305e23ce02
Jiri Vestfal <45360788+MissingNO57@users.noreply.github.com>
2025-11-20 11:57:51 +0100

    Gemini upgrade tag (#424)
    Added Gemini upgrade tag

--- app/upgrades.go
+++ app/upgrades.go
@@ -36,7 +36,7 @@ import (
 )
 
 // ---- Match this to the plan name that is already stored on disk and halted the chain.
-const UpgradeNameV053 = "v0.14.0" // <-- CHANGE if your plan name is different.
+const UpgradeNameV053 = "v0.15.0-rc2-gemini"
 
 // List ALL new/renamed/deleted KV stores at this upgrade height.
 var v053StoreUpgrades = storetypes.StoreUpgrades{
